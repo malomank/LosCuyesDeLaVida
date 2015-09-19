8c12300f07c90346b16348bc56c1a170d64a8e5f
Carlo Alva <carlo0071@gmail.com>
2015-09-19 14:32:33 -0500

    Acciones especiales sin constructor y combinaciones

--- src/Modelo/AccionesEspeciales.java
+++ src/Modelo/AccionesEspeciales.java
@@ -1,5 +1,9 @@
 package Modelo;
 
 public class AccionesEspeciales {
-
+	
+//	individuales arreglo de combinaciones
+	private Combinaciones [] individuales;
+//	enconjunto   arreglo de combinaciones (la clase que est arriba )
+	private Combinaciones [] duo;
 }
