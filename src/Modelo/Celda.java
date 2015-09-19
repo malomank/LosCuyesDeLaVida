ec0eff153ee6b290c2f3e83b1172e1e4845d542d
Carlo Alva <carlo0071@gmail.com>
2015-09-19 14:15:42 -0500

    Se agregó Clase Posición a Celda

--- src/Modelo/Celda.java
+++ src/Modelo/Celda.java
@@ -1,5 +1,24 @@
 package Modelo;
 
 public class Celda {
-
+	private char caracter ;
+	private Posicion pos;
+	
+	public Celda(){
+		this.setCaracter(' ');
+		pos = new Posicion(0,0);
+	}
+	
+	public Celda(int fil, int col, char caracter){
+		this.setCaracter(caracter);
+		pos = new Posicion(fil,col);
+	}
+	
+	private char getCaracter() {
+		return caracter;
+	}
+	private void setCaracter(char caracter) {
+		this.caracter = caracter;
+	}
+	
 }
