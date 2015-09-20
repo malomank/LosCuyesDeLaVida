84dc54b965b5cd1162e185b1e46e959f0ba67ed1
camila barboza <campivi13@gmail.com>
2015-09-20 13:50:29 -0500

    Personajes
    tiene los set/gets y los constructores

--- src/Modelo/PersonajeSecundario.java
+++ src/Modelo/PersonajeSecundario.java
@@ -1,5 +1,22 @@
 package Modelo;
 
 public class PersonajeSecundario extends Personaje {
+	private static int vida ;
+	private  String nombre; 
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
+	public static int getVida() {
+		return vida;
+	}
+	
+	public PersonajeSecundario(String nombre , int ancho , int alto , int posX, int posY, char letra, boolean visible , boolean atravesable){
+		super.setMainValues(ancho, alto, posX, posY, letra, visible , atravesable);
+		vida = 10;		
+	}
 }
