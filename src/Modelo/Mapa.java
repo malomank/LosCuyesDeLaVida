01527998877c35e12520433ff77596145c478700
Joe Natan <a20105726@pucp.pe>
2015-09-20 00:04:40 -0500

    Mapas y Objetos

--- src/Modelo/Mapa.java
+++ src/Modelo/Mapa.java
@@ -1,5 +1,50 @@
 package Modelo;
 
 public class Mapa {
+	private AccionesEspeciales acciones;
+	private Celda [][] mapa = new Celda[12][16];
+	private int estadoDuo ; 
+	private int contador ; 
+	
+	public Mapa(){		//Inicializa el mapa con caracteres en blanco
+		for(int i=0;i<12;i++){
+			for(int j=0;j<16;j++){
+				mapa[i][j] = new Celda(' ', i, j);
+			}
+		}
+	}
+	
+	public void ImprimirMapa(){ //Imprime mapa para verificar que estaba bien cargado
+		for(int i=0;i<12;i++){
+			for(int j=0;j<16;j++){
+				System.out.print(mapa[i][j].getCaracter());
+			}
+			System.out.println();
+			}
+	}
 
+	public int getContador() {
+		return contador;
+	}
+
+	public void setContador(int contador) {
+		this.contador = contador;
+	}
+
+	public AccionesEspeciales getAcciones() {
+		return acciones;
+	}
+
+	public void setAcciones(AccionesEspeciales acciones) {
+		this.acciones = acciones;
+	}
+
+	public int getEstadoDuo() {
+		return estadoDuo;
+	}
+
+	public void setEstadoDuo(int estadoDuo) {
+		this.estadoDuo = estadoDuo;
+	}
+	
 }
--- src/Modelo/Objeto.java
+++ src/Modelo/Objeto.java
@@ -1,5 +1,92 @@
 package Modelo;
 
-public class Objeto {
+import java.util.ArrayList;
 
+public class Objeto {
+	
+	   private boolean  visible; 
+	   private boolean  atravesable;
+	   
+	   private int posX; 
+	   private int posY;
+	   
+	   private Sprite matriz[][] ;
+	   private ArrayList<Integer> estados;  
+	   private int estadoActual;
+	   
+	   
+	   public Objeto(boolean visible , boolean atravesable , int posX , int posY){ 
+		   // Constructor 
+		   this.visible = visible ; 
+		   this.atravesable = atravesable ; 
+		   this.posX = posX ; 
+		   this.posY = posY ; 	   
+		   estados = new ArrayList<Integer>(10);
+		   for(int i = 0 ; i<=10 ; i++) getEstados().add(i) ;   // Añados estados del 0 a. 9  
+			
+		    // Considero 10 estados por 20 imagenes
+			matriz  = new Sprite[10][20];
+			
+			setEstadoActual(getEstados().get(0)) ; // estado inicial es el el valor del priver indice
+	   }
+	   public void SetAllValues(boolean visible , boolean atravesable , int posX , int posY) {
+		   // Seteo de todos los valores 
+		   setPosX(posX);
+		   setPosY(posY);
+		   setAtravesable(atravesable);
+		   setVisible(visible);	   
+	   }   
+	   
+	   public void agregarSprites(ArrayList<Sprite> sprites, int estado ){
+			for(int i = 0 ; i<sprites.size() ; i++){
+			   matriz[estado][i] = new Sprite();
+			   // Aqui se debe agregar la referencia a sprite que se carga
+			   
+			}
+		}
+	   
+	public boolean isVisible() {
+		return visible;
+	}
+	public void setVisible(boolean visible) {
+		this.visible = visible;
+	}
+	public boolean isAtravesable() {
+		return atravesable;
+	}
+	public void setAtravesable(boolean atravesable) {
+		this.atravesable = atravesable;
+	}
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	public ArrayList<Integer> getEstados() {
+		return estados;
+	}
+	public void setEstados(ArrayList<Integer> estados) {
+		this.estados = estados;
+	}
+	public int getEstadoActual() {
+		return estadoActual;
+	}
+	public void setEstadoActual(int estadoActual) {
+		this.estadoActual = estadoActual;
+	}
+	public Sprite[][] getMatriz() {
+		return matriz;
+	}
+	public void setMatriz(Sprite matriz[][]) {
+		this.matriz = matriz;
+	}
+	   
+	   
 }
