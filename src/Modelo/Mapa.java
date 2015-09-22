edd50abd1a247044a307acb8dcc2196edf9c0698
brayanrp215 <brayanrp215@gmail.com>
2015-09-22 00:45:41 -0500

    Funca Nro 2:
    Clase Juego, pero sin duos

--- src/Modelo/Mapa.java
+++ src/Modelo/Mapa.java
@@ -1,5 +1,59 @@
 package Modelo;
 
-public class Mapa {
 
+public class Mapa{
+	private AccionesEspeciales acciones;
+	private Celda [][] mapa = new Celda[12][16];
+	private int estadoDuo ; 
+	private int contador ; 
+	
+	public Mapa(){		//Inicializa el mapa con caracteres en blanco
+		for(int fil=0;fil<12;fil++){
+			for(int col=0;col<16;col++){
+				mapa[fil][col] = new Celda( fil, col,' ');
+			}
+		}
+	}
+	//funcion 
+	public void establecerCaracter(int i, int j, char x){
+		mapa[i][j].setCaracter(x);
+	}
+	
+	public char obtenerCaracter(int fil, int col){
+		return mapa[fil][col].getCaracter();
+	}
+	
+	public void ImprimirMapa(){ //Imprime mapa para verificar que estaba bien cargado
+		for(int fil=0;fil<12;fil++){
+			for(int col=0;col<16;col++){
+				System.out.print(mapa[fil][col].getCaracter());
+			}
+			System.out.println();
+			}
+	}
+
+	public int getContador() {
+		return contador;
+	}
+
+	public void setContador(int contador) {
+		this.contador = contador;
+	}
+
+	public AccionesEspeciales getAcciones() {
+		return acciones;
+	}
+
+	public void setAcciones(AccionesEspeciales acciones) {
+		this.acciones = acciones;
+	}
+
+	public int getEstadoDuo() {
+		return estadoDuo;
+	}
+
+	public void setEstadoDuo(int estadoDuo) {
+		this.estadoDuo = estadoDuo;
+	}
+	
 }
---
+++ src/Principal.java
@@ -0,0 +1,14 @@
+import Controlador.*;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+ 	    Juego nuevoJuego = new Juego(10, 50,40) ; 
+ 	    nuevoJuego.iniciarPersonajes();
+ 	    nuevoJuego.PantallaInicial(  ); 	    	    		
+		
+	}
+
+}
